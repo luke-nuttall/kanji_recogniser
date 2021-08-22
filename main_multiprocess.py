9e0cd44dd722fff2faad29e61165dfde1d218b0b
luke-nuttall <87031065+luke-nuttall@users.noreply.github.com>
2021-08-22 16:20:02 +0100

    Factor out model-building code into file models.py
    Previously there was a lot of code duplication across the three versions
    of the program. This change removes the identical model-building code
    from those three versions and puts it in a single file which is imported
    into each of the others.

--- main_multiprocess.py
+++ main_multiprocess.py
@@ -12,7 +12,8 @@ from matplotlib import pyplot as plt, font_manager
 import numpy as np
 
 import tensorflow as tf
-from tensorflow.keras import layers
+
+from models import build_recogniser
 
 font_manager.fontManager.addfont("fonts/NotoSansJP-Regular.otf")
 plt.rc('font', family='Noto Sans JP')
@@ -238,74 +239,6 @@ def build_dataset(buf: CircularBuffer):
     ))
 
 
-class ResConv2D(layers.Layer):
-    """
-    Crude implementation of a ResNet unit with two convolutional layers.
-    In this version the skip connection bypasses the activation function.
-    UPDATE: Can now handle changes in filter size!
-    """
-
-    def __init__(self, filters: int, kernel_size: int, activation="relu", name=None):
-        self.filters = filters
-        super().__init__(name=name)
-        self.conv1 = layers.Conv2D(filters, kernel_size, padding="same")
-        self.act1 = layers.Activation(activation)
-        self.conv2 = layers.Conv2D(filters, kernel_size, padding="same")
-        self.act2 = layers.Activation(activation)
-        self.diff_f = None
-
-    def build(self, input_shape):
-        # assume shape: [batch, y, x, filter]
-        in_filters = input_shape[-1]
-        self.diff_f = self.filters - in_filters
-
-    def _add_skip_connection(self, input, x):
-        # return x + input
-        if self.diff_f > 0:
-            paddings = [[0, 0], [0, 0], [0, 0], [0, self.diff_f]]
-            skip = tf.pad(input, paddings, "CONSTANT")
-        elif self.diff_f < 0:
-            skip = input[:, :, :, :self.filters]
-        else:
-            skip = input
-        return x + skip
-
-    def call(self, inputs, *args, **kwargs):
-        x = self.conv1(inputs)
-        x = self.act1(x)
-        x = self.conv2(x)
-        x = self.act2(x)
-        x = self._add_skip_connection(inputs, x)
-        return x
-
-
-def build_regulariser():
-    l_input = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1))
-    l_next = layers.Conv2D(16, 3, activation="relu", name="conv1")(l_input)
-    l_next = layers.Conv2D(64, 3, activation="relu", name="conv2")(l_next)
-    l_next = layers.MaxPool2D()(l_next)
-    l_next = layers.Flatten()(l_next)
-    l_next = layers.Dense(64, activation="relu", name="dense1")(l_next)
-    l_angle = layers.Dense(CATEGORIES_ANGLE, activation="softmax")(l_next)
-    l_size = layers.Dense(1)(l_next)
-    model = tf.keras.Model(inputs=[l_input], outputs=[l_angle])
-    return model
-
-
-def build_recogniser(depth: int):
-    l_input = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1))
-    l_next = layers.Conv2D(32, 5, activation="relu", padding="same", name="conv1")(l_input)
-    l_next = layers.MaxPool2D()(l_next)
-    for ii in range(depth):
-        l_next = ResConv2D(64, 3, name=f"resblock_{ii+1}")(l_next)
-    l_next = layers.AveragePooling2D()(l_next)
-    l_next = layers.Flatten()(l_next)
-    l_next = layers.Dense(64, activation="relu", name="dense1")(l_next)
-    l_kanji = layers.Dense(CATEGORIES_KANJI, activation="softmax")(l_next)
-    model = tf.keras.Model(inputs=[l_input], outputs=[l_kanji], name="recogniser")
-    return model
-
-
 def plot_sample_from_dataset(dataset):
     fig = plt.figure(figsize=(8, 8))
     for ii, row in enumerate(dataset.take(16)):
@@ -332,7 +265,7 @@ def main():
     time.sleep(5)
     plot_sample_from_dataset(dataset)
 
-    m_kanji = build_recogniser(10)
+    m_kanji = build_recogniser(10, IMG_SIZE, CATEGORIES_KANJI)
     m_kanji.summary()
     m_kanji.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
---
+++ models.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+class ResConv2D(layers.Layer):
+    """
+    Crude implementation of a ResNet unit with two convolutional layers.
+    In this version the skip connection bypasses the activation function.
+    UPDATE: Can now handle changes in filter size!
+    """
+
+    def __init__(self, filters: int, kernel_size: int, activation="relu", name=None):
+        self.filters = filters
+        super().__init__(name=name)
+        self.conv1 = layers.Conv2D(filters, kernel_size, padding="same")
+        self.act1 = layers.Activation(activation)
+        self.conv2 = layers.Conv2D(filters, kernel_size, padding="same")
+        self.act2 = layers.Activation(activation)
+        self.diff_f = None
+
+    def build(self, input_shape):
+        # assume shape: [batch, y, x, filter]
+        in_filters = input_shape[-1]
+        self.diff_f = self.filters - in_filters
+
+    def _add_skip_connection(self, inputs, x):
+        if self.diff_f > 0:
+            paddings = [[0, 0], [0, 0], [0, 0], [0, self.diff_f]]
+            skip = tf.pad(inputs, paddings, "CONSTANT")
+        elif self.diff_f < 0:
+            skip = inputs[:, :, :, :self.filters]
+        else:
+            skip = inputs
+        return x + skip
+
+    def call(self, inputs, *args, **kwargs):
+        x = self.conv1(inputs)
+        x = self.act1(x)
+        x = self.conv2(x)
+        x = self.act2(x)
+        x = self._add_skip_connection(inputs, x)
+        return x
+
+
+def build_regulariser(image_size: int, angle_categories: int):
+    l_input = layers.Input(shape=(image_size, image_size, 1))
+    l_next = layers.Conv2D(16, 3, activation="relu", name="conv1")(l_input)
+    l_next = layers.Conv2D(64, 3, activation="relu", name="conv2")(l_next)
+    l_next = layers.MaxPool2D()(l_next)
+    l_next = layers.Flatten()(l_next)
+    l_next = layers.Dense(64, activation="relu", name="dense1")(l_next)
+    l_angle = layers.Dense(angle_categories, activation="softmax")(l_next)
+    l_size = layers.Dense(1)(l_next)
+    model = tf.keras.Model(inputs=[l_input], outputs=[l_angle, l_size])
+    return model
+
+
+def build_recogniser(depth: int, image_size: int, kanji_categories: int):
+    l_input = layers.Input(shape=(image_size, image_size, 1))
+    l_next = layers.Conv2D(32, 5, activation="relu", padding="same", name="conv1")(l_input)
+    l_next = layers.MaxPool2D()(l_next)
+    for ii in range(depth):
+        l_next = ResConv2D(64, 3, name=f"resblock_{ii+1}")(l_next)
+    l_next = layers.AveragePooling2D()(l_next)
+    l_next = layers.Flatten()(l_next)
+    l_next = layers.Dense(64, activation="relu", name="dense1")(l_next)
+    l_kanji = layers.Dense(kanji_categories, activation="softmax")(l_next)
+    model = tf.keras.Model(inputs=[l_input], outputs=[l_kanji], name="recogniser")
+    return model
