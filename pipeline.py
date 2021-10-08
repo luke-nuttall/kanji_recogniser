7c98b1bb26822f7dc86c6e97a9446001f6192175
luke-nuttall <87031065+luke-nuttall@users.noreply.github.com>
2021-10-08 20:13:19 +0100

    Refactor Provider classes to remove duplicate code
    There was a lot of code duplication between ProviderMultithread and
    ProviderMultiprocess. This commit removes that redundancy.

--- pipeline.py
+++ pipeline.py
@@ -23,35 +23,32 @@ class GenThread(threading.Thread):
         super().__init__()
         self.queue = queue
         self.__kanji = kanji
-        self.halt = False
+        self.__halt = False
         self.lock = threading.Lock()
         self.__fonts = fonts
         self.batch_size = batch_size
 
     def run(self):
         print(f"{self.__class__.__name__} has started.")
-        while not self.halt:
+        while not self.__halt:
             with self.lock:
                 batch = [gen_training_sample(self.__kanji, self.__fonts) for _ in range(self.batch_size)]
             self.queue.put(batch)
         print(f"{self.__class__.__name__} has stopped.")
 
-    @property
-    def kanji(self):
-        with self.lock:
-            return self.__kanji
-
-    @kanji.setter
-    def kanji(self, values):
+    def set_kanji(self, values):
         with self.lock:
             self.__kanji = values
+
+    def halt(self):
+        self.__halt = True
 
 
 class GenProcess(mp.Process):
     def __init__(self, queue: mp.Queue, kanji: list, fonts: list, batch_size=16):
         super().__init__()
         self.__kanji = kanji
-        self.halt = mp.Value('b', False)  # shared memory
+        self.__halt = mp.Value('b', False)  # shared memory
         self.__fonts = fonts
         self.conn_parent, self.conn_child = mp.Pipe()
         self.queue = queue
@@ -61,7 +58,7 @@ class GenProcess(mp.Process):
 
     def run(self):
         print(f"{self.__class__.__name__} has started.")
-        while not self.halt.value:
+        while not self.__halt.value:
             while self.conn_child.poll():
                 self.__kanji = self.conn_child.recv()
             batch = [gen_training_sample(self.__kanji, self.__fonts) for _ in range(self.__batch_size)]
@@ -70,6 +67,9 @@ class GenProcess(mp.Process):
 
     def set_kanji(self, values):
         self.conn_parent.send(values)
+
+    def halt(self):
+        self.__halt.value = True
 
 
 class Provider:
@@ -111,13 +111,19 @@ class Provider:
         return get_dataset_from_generator(self.__generator)
 
 
-class ProviderMultithread(Provider):
+class ProviderMultiBase(Provider):
     def __init__(self, kanji: list, fonts: list, n_threads=4):
         super().__init__(kanji, fonts)
         self.queue_size = 32
         self.manager = mp.Manager()  # see https://stackoverflow.com/a/46041587
         self.queue = self.manager.Queue(self.queue_size)
-        self.threads = [GenThread(self.queue, kanji, fonts) for _ in range(n_threads)]
+        self.threads = self.create_threads(kanji, fonts, n_threads)
+
+    def create_threads(self, kanji, fonts, n_threads: int) -> list:
+        """
+        Subclasses should override this to return a new list of thread objects
+        """
+        return []
 
     def start_background_tasks(self):
         for thread in self.threads:
@@ -125,8 +131,8 @@ class ProviderMultithread(Provider):
 
     def stop_background_tasks(self):
         for thread in self.threads:
-            thread.halt = True
-        self.__flush_queue()  # This just makes sure that the processes aren't stuck waiting for space in the queue.
+            thread.halt()
+        self.__flush_queue()  # This just makes sure that the threads aren't stuck waiting for space in the queue.
         for thread in self.threads:
             thread.join()
 
@@ -140,68 +146,6 @@ class ProviderMultithread(Provider):
                 self.queue.get(block=False)
             except queue.Empty:
                 return
-
-    @property
-    def kanji(self):
-        return self.__kanji
-
-    @kanji.setter
-    def kanji(self, values):
-        self.__kanji = values
-        for thread in self.threads:
-            thread.kanji = values
-        self.__flush_queue()
-
-    def __generator(self):
-        while True:
-            batch = self.queue.get(timeout=5)
-            for sample in batch:
-                yield sample
-
-    def get_dataset(self) -> tf.data.Dataset:
-        return get_dataset_from_generator(self.__generator)
-
-
-class ProviderMultiprocess(Provider):
-    def __init__(self, kanji: list, fonts: list, n_threads=4):
-        super().__init__(kanji, fonts)
-        self.queue_size = 32
-        self.manager = mp.Manager()  # see https://stackoverflow.com/a/46041587
-        self.queue = self.manager.Queue(self.queue_size)
-        self.procs = [GenProcess(self.queue, kanji, fonts) for _ in range(n_threads)]
-
-    def start_background_tasks(self):
-        for proc in self.procs:
-            proc.start()
-
-    def stop_background_tasks(self):
-        for proc in self.procs:
-            proc.halt.value = True
-        self.__flush_queue()  # This just makes sure that the processes aren't stuck waiting for space in the queue.
-        for proc in self.procs:
-            proc.join()
-
-    def __flush_queue(self):
-        """
-        Remove every item which is in the queue when this function is called.
-        Items which are added to the queue while this function is running may or may not be removed.
-        """
-        for ii in range(self.queue_size):
-            try:
-                self.queue.get(block=False)
-            except queue.Empty:
-                return
-
-    @property
-    def kanji(self):
-        return self.__kanji
-
-    @kanji.setter
-    def kanji(self, values):
-        self.__kanji = values
-        for proc in self.procs:
-            proc.set_kanji(values)
-        self.__flush_queue()  # Clear old data so that newly generated samples are at the front of the queue.
 
     def __generator(self):
         while True:
@@ -220,3 +164,13 @@ class ProviderMultiprocess(Provider):
         # Maybe the code path to call a method which has been overridden in a child class is much more complicated
         # than the code path when calling a method which is defined in the same class.
         # It might be similar to the concept of virtual methods in low-level languages.'''
+
+
+class ProviderMultithread(ProviderMultiBase):
+    def create_threads(self, kanji, fonts, n_threads: int):
+        return [GenThread(self.queue, kanji, fonts) for _ in range(n_threads)]
+
+
+class ProviderMultiprocess(ProviderMultiBase):
+    def create_threads(self, kanji, fonts, n_threads: int):
+        return [GenProcess(self.queue, kanji, fonts) for _ in range(n_threads)]
